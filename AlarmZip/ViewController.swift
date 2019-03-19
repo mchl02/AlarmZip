91f3ee0f1e673f190da6afdc5df802a633d7d920
Matthew Lee <matthewchlee02@gmail.com>
2019-03-18 23:29:04 -0400

    Enhanced GUI and added notifications

--- AlarmZip/ViewController.swift
+++ AlarmZip/ViewController.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
-//testing change?
+    
+    @IBAction func action(_ sender: Any)
+    {
+        let content = UNMutableNotificationContent()
+        content.title = "How many days are there in one year"
+        content.subtitle = "Do you know?"
+        content.body = "Do you really know?"
+        content.badge = 1
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        let request = UNNotificationRequest(identifier: "timerDone", content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //Asked for permission
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge], completionHandler: {didAllow, error in
+        })
     }
-
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
 }
+
 
